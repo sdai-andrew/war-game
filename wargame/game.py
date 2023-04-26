464808aab788729628b8f9347a5bf5e6e2a14b3a
sdai-andrew <sdai2@andrew.cmu.edu>
2023-04-26 12:47:13 -0400

    Updated move serialization for easier UI parsing

--- wargame/game.py
+++ wargame/game.py
@@ -31,8 +31,9 @@ class Game():
         self.player2 = Player(deque(deck[len(deck) // 2 :]))
         self.moves = []
     
-    def serializeMove(self, p1Card, p2Card, p1FaceUp, p2FaceUp):
+    def serializeMove(self, status, p1Card, p2Card, p1FaceUp, p2FaceUp):
         result = {}
+        result["Status"] = status
         result["p1NumCards"] = len(self.player1.deck)
         result["p2NumCards"] = len(self.player2.deck)
         oneCard = {}
@@ -62,39 +63,50 @@ class Game():
 
     def play(self):
         # add move to indicate start of game
-        self.serializeMove(None, None, None, None)
+        self.serializeMove("Starting Game", None, None, None, None)
         war = []
         while self.player1.deck and self.player2.deck:
             player1Card = self.player1.deck.popleft()
             player2Card = self.player2.deck.popleft()
             war.extend([player1Card, player2Card])
-            self.serializeMove(player1Card, player2Card, True, True)
             if player1Card.value > player2Card.value:
                 self.player1.deck.extend(war)
+                self.serializeMove("Player 1 victory", player1Card, player2Card, True, True)
             elif player2Card.value > player1Card.value:
                 self.player2.deck.extend(war)
+                self.serializeMove("Player 2 victory", player1Card, player2Card, True, True)
             else:
+                self.serializeMove("Tie, time for war!", player1Card, player2Card, True, True)
                 isWar = True
                 while isWar:
                     # add face down cards
+                    self.serializeMove("Tie, time for war!", None, None,
+                                       None if len(self.player1.deck) == 0 else False,
+                                       None if len(self.player1.deck) == 0 else False)
                     if self.player1.deck:
                         war.append(self.player2.deck.popleft())
                     if self.player2.deck:
                         war.append(self.player2.deck.popleft())
-                    self.serializeMove(None, None, False, False)
                     # add face up cards
                     if self.player1.deck:
                         player1Card = self.player2.deck.popleft()
                     if self.player2.deck:
                         player2Card = self.player2.deck.popleft()
                     war.extend([player1Card, player2Card])
-                    self.serializeMove(player1Card, player2Card, True, True)
+                    status = ""
                     if player1Card.value > player2Card.value:
                         self.player1.deck.extend(war)
+                        status = "Player 1 victory"
                         isWar = False
                     elif player2Card.value > player1Card.value:
                         self.player2.deck.extend(war)
+                        status = "Player 2 victory"
                         isWar = False
+                    else:
+                        status = "Tie again, time for war!"
+                    self.serializeMove(status, player1Card, player2Card, True, True)
+        status = "Player 1 won!" if len(self.player2.deck) == 0 else "Player 2 won!"
+        self.serializeMove(status, None, None, None, None)
         return self.moves            
 
 class Card():
@@ -102,6 +114,13 @@ class Card():
         self.suit = suit
         self.color = color
         self.value = value
+    
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__) and other.suit == self.suit and
+                other.color == self.color and other.value == self.value)
+
+    def __hash__(self):
+        return hash(self.suit + self.color + str(self.value))
 
 class Player():
     def __init__(self, deck):
