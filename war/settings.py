a85bc3b072f314b1b34d031f24e1d6243e58a7ae
sdai-andrew <sdai2@andrew.cmu.edu>
2023-04-28 04:58:08 +0000

    Added back ip in allowed hosts

--- war/settings.py
+++ war/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = CONFIG.get("Django", "Secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.136.19.125']
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/'
